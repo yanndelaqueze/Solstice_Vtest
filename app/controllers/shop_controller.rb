5139972f01d24e75d433c046c990315b32debf8f
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-07-04 14:59:33 +0200

    index products

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,6 +1,13 @@
 class ShopController < ApplicationController
+  before_action :set_cats
   def index
     @page = 'shop'
     @products = Product.all
+  end
+
+  private
+
+  def set_cats
+    @cats = Category.all.where(display: true)
   end
 end
